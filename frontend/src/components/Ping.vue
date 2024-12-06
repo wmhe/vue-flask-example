d16ad278f63129c0d878a30dea03bb63c361c2bf
wmhe <william.he56@gmail.com>
2024-12-05 18:37:27 -0800

    Setup Vue

---
+++ frontend/src/components/Ping.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <button>{{ msg }}</button>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'Ping',
+    data() {
+        return {
+            msg: "Hello!",
+        };
+    },
+    methods: {
+        getMessage() {
+            const path = 'http://localhost:5001/ping';
+            axios.get(path)
+            .then((res) => {
+                this.msg = res.data;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+        },
+    },
+    created() {
+        this.getMessage();
+    },
+};
+</script>
