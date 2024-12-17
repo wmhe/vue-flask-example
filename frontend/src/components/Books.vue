1c592feba4ec10b84c5a62c544c53adeb4131179
wmhe <william.he56@gmail.com>
2024-12-16 16:09:01 -0800

    Add PUT support

--- frontend/src/components/Books.vue
+++ frontend/src/components/Books.vue
@@ -30,7 +30,7 @@
               </td>
               <td>
                 <div class="btn-group" role="group">
-                  <button type="button" class="btn btn-warning btn-sm">Update</button>
+                  <button type="button" class="btn btn-warning btn-sm" @click="toggleEditBookModal(book)">Update</button>
                   <button type="button" class="btn btn-danger btn-sm">Delete</button>
                 </div>
               </td>
@@ -108,6 +108,76 @@
       </div>
     </div>
     <div v-if="activeAddBookModal" class="modal-backdrop fade show"></div>
+
+
+    <!-- edit book modal -->
+    <div
+      ref="editBookModal"
+      class="modal fade"
+      :class="{ show: activeEditBookModal, 'd-block': activeEditBookModal }"
+      tabindex="-1"
+      role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Update</h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+              @click="toggleEditBookModal">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="mb-3">
+                <label for="editBookTitle" class="form-label">Title:</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="editBookTitle"
+                  v-model="editBookForm.title"
+                  placeholder="Enter title">
+              </div>
+              <div class="mb-3">
+                <label for="editBookAuthor" class="form-label">Author:</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="editBookAuthor"
+                  v-model="editBookForm.author"
+                  placeholder="Enter author">
+              </div>
+              <div class="mb-3 form-check">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  id="editBookRead"
+                  v-model="editBookForm.read">
+                <label class="form-check-label" for="editBookRead">Read?</label>
+              </div>
+              <div class="btn-group" role="group">
+                <button
+                  type="button"
+                  class="btn btn-primary btn-sm"
+                  @click="handleEditSubmit">
+                  Submit
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-danger btn-sm"
+                  @click="handleEditCancel">
+                  Cancel
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-if="activeEditBookModal" class="modal-backdrop fade show"></div>
   </div>
 </template>
 
@@ -119,11 +189,19 @@ export default {
     return {
       activeAddBookModal: false,
       addBookForm: {
+        id: '',
         title: '',
         author: '',
         read: [],
       },
       books: [],
+      activeEditBookModal: false,
+      editBookForm: {
+        id: '',
+        title: '',
+        author: '',
+        read: [],
+      },
     };
   },
   methods: {
@@ -171,6 +249,10 @@ export default {
       this.addBookForm.title = '';
       this.addBookForm.author = '';
       this.addBookForm.read = [];
+      this.editBookForm.id = '';
+      this.editBookForm.title = '';
+      this.editBookForm.author = '';
+      this.editBookForm.read = [];
     },
     toggleAddBookModal() {
       const body = document.querySelector('body');
@@ -180,6 +262,47 @@ export default {
       } else {
         body.classList.remove('modal-open');
       }
+    },
+    toggleEditBookModal(book) {
+      if (book) {
+        this.editBookForm = book;
+      }
+      const body = document.querySelector('body');
+      this.activeEditBookModal = !this.activeEditBookModal;
+      if (this.activeEditBookModal) {
+        body.classList.add('modal-open');
+      } else{
+        body.classList.remove('modal-open');
+      }
+    },
+    handleEditSubmit() {
+      this.toggleEditBookModal(null);
+      let read = false;
+      if (this.editBookForm.read) read = true;
+      const payload = {
+        title: this.editBookForm.title,
+        author: this.editBookForm.author,
+        read,
+      };
+      this.updateBook(payload, this.editBookForm.id);
+    },
+    updateBook(payload, bookID) {
+      const path = `http://localhost:5001/books/${bookID}`;
+      axios.put(path, payload)
+        .then(() => {
+          this.getBooks();
+          this.message = 'Book updated!';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          console.error(error);
+          this.getBooks();
+        });
+    },
+    handleEditCancel() {
+      this.toggleEditBookModal(null);
+      this.initForm();
+      this.getBooks(); // why?
     },
   },
   created() {
